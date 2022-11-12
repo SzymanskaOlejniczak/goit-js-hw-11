7ebae3b409c84042dac3aa49c3230c773b74b188
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-11-12 20:34:44 +0100

    Update markup.js

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,22 +1,42 @@
-export function galleryMarkup(array) {
-    return array.reduce((acc, {largeImageURL, webformatURL, tags, likes, views, comments, downloads}) => acc + `
-    <a href="${largeImageURL}" class="gallery__link">
-    <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-            <p class="info-item">
-                <b>Likes</b> ${likes}
-            </p>
-            <p class="info-item">
-                <b>Views</b> ${views}
-            </p>
-            <p class="info-item">
-                <b>Comments</b> ${comments}
-            </p>
-            <p class="info-item">
-                <b>Downloads</b> ${downloads}
-            </p>
-        </div>
-    </div>
-    </a>`, " ");
+
+// import SipleLighbox
+import SimpleLightbox from 'simplelightbox';
+// additional styles import
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+// render images
+export function renderImages(images) {
+  const markup = images
+    .map(image => {
+      return `<div class="photo-card">
+                  <a class="gallery__item" href="${image.largeImageURL}">
+                    <img class="gallery__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
+                  </a>
+                  <div class="info">
+                    <p class="info-item">
+                      <b>Likes</b><br>
+                      ${image.likes}
+                    </p>
+                    <p class="info-item">
+                      <b>Views</b><br>
+                      ${image.views}
+                    </p>
+                    <p class="info-item">
+                      <b>Comments</b><br>
+                      ${image.comments}
+                    </p>
+                    <p class="info-item">
+                      <b>Downloads</b><br>
+                      ${image.downloads}
+                    </p>
+                  </div>
+              </div>`;
+    })
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+
+  // add simpleLightbox library
+  var lightbox = new SimpleLightbox('.gallery a');
 }
