e042cd9c1b5567a79fbbe8912fba18e174a1448d
SzymanskaOlejniczak <aszymanskaolejniczak@gmail.com>
2022-11-12 20:34:40 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,65 +1,46 @@
-import SimpleLightbox from 'simplelightbox';
-import "simplelightbox/dist/simple-lightbox.min.css";
+// impoer Notiflix
 import Notiflix from 'notiflix';
-import { galleryMarkup } from "./js/markup";
-import { searchQuery } from "./js/fetch";
 
-const searchForm = document.querySelector('.js-search-form'),
-    gallery = document.querySelector('.js-gallery'),
-    loadMore = document.querySelector('.js-load-more'),
-    lightbox = new SimpleLightbox('.gallery a', { CaptionDelay: 250, captions: true, captionsData: 'alt' });
+// import functions and variables
+import { fetchImages, lastIndexPage } from './js/fetch';
+import { renderImages } from './js/markup';
 
-searchForm.addEventListener('submit', searchInformation);
-loadMore.addEventListener('click', onButtonClick);
+const gallery = document.querySelector('.gallery');
+const inputTag = document.querySelector('#search-form input');
+const searchBtn = document.querySelector('#search-button');
+const loadMoreBtn = document.querySelector('#load-more');
 
-async function searchInformation(event) {
-    event.preventDefault();
-    loadMore.classList.add('is-hidden');
-    searchQuery.page = 1;
+export let page = 1;
 
-    const query = event.target.elements.searchQuery.value.trim(),
-        response = await searchQuery.searchPictures(query),
-        galleryItem = response.hits;
 
-    try {
-        gallery.innerHTML = '';
-        if (galleryItem.length === 0) {
-            Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.");
-        }  else if (!query) {
-                return Notiflix.Notify.info('Please, enter key word for search!');
-        } else {
-            Notiflix.Notify.success(`Hooray! We found ${response.totalHits} images.`);
-            renderingMarkup(response.hits);
-            loadMore.classList.remove('is-hidden');
-        } 
+const search = () => {
+  event.preventDefault();
+  loadMoreBtn.style.display = 'none';
 
-    } catch (error) {
-        console.log(error.message);
-    }
-    console.log(response);
-}
+  const name = inputTag.value.trim();
 
-async function onButtonClick() {
-    searchQuery.page += 1;
+  fetchImages(name, page)
+    .then(images => {
+      renderImages(images.hits);
+      if (page < lastIndexPage) {
+        loadMoreBtn.style.display = 'block';
+      }
+    })
+    .catch(error => console.log(error));
+};
 
-    const response = await searchQuery.searchPictures();
-    if (searchQuery.page > response.totalHits / searchQuery.per_page) {
-        loadMore.classList.add('is-hidden');
-        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-    }
-    renderingMarkup(response.hits);
+searchBtn.addEventListener('click', () => {
+  event.preventDefault();
 
-    const { height: cardHeight } = document
-        .querySelector(".gallery")
-        .firstElementChild.getBoundingClientRect();
+  if (inputTag.value.trim().length >= 1) {
+    gallery.innerHTML = '';
+    search();
+  } else {
+    Notiflix.Notify.failure('Please enter something.');
+  }
+});
 
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: "smooth",
-    });
-}
-
-function renderingMarkup(array) {
-    gallery.insertAdjacentHTML('beforeend', galleryMarkup(array));
-    lightbox.refresh();
-}
+loadMoreBtn.addEventListener('click', () => {
+  page = page + 1;
+  search();
+});
